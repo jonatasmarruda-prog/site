587e51aa6956ccd17b39e9782b3f958d522ef5f7
jonatasmarruda-prog <jonatasmarruda@gmail.com>
2026-09-04 14:55:13 -0400

    Atualizar cache PWA para versao 8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='trilheiros-gps-v7.0.0';
+const CACHE='trilheiros-gps-v8.0.0';
 const ASSETS=['./','index.html','app.css?v=7','app.js?v=7','enhance.js?v=7','manifest.webmanifest?v=7','assets/logo-trilheiros.png','assets/icons/icon-192.png','assets/icons/icon-512.png','assets/icons/apple-touch-icon.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
